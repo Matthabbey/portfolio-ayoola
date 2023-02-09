d338e072d045ae3d46a73fc765b112e0605a9b02
Akinpelu Abiodun <59386529+Matthabbey@users.noreply.github.com>
2023-02-09 22:47:36 +0100

    navbar

---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Navbar, Container, Nav } from 'react-bootstrap'
+
+const NavBar = () => {
+  return (
+        <Navbar expand="lg">
+
+            <Container>
+                <Navbar.Brand href='#home'>
+                    <img src={""} alt="" />
+                </Navbar.Brand>
+                <Navbar.Toggle>
+                    <span className="navbar-toggle-icon"></span>
+                </Navbar.Toggle>
+                <Navbar.Collapse>
+                    <Nav className="me-auto">
+                        <Nav.Link href='#home'>Home</Nav.Link>
+                        <Nav.Link href='#skills'>Skills </Nav.Link>
+                        <Nav.Link href='#projects'>Projects </Nav.Link>
+                    </Nav>
+                    <span className="navbar-text">
+                        <div className="social-icons">
+                            <a href="#"></a>
+                            <a href="#"></a>
+                            <a href="#"></a>
+                        </div>
+                        <button onClick={()=> console.log()}><span>Let's Connect</span></button>
+                    </span>
+                </Navbar.Collapse>
+            </Container>
+
+        </Navbar>
+  )
+}
+
+export default Navbar
