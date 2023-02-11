e8fdcd399979dbc213323cbe335e0885a8a35e95
Akinpelu Abiodun <59386529+Matthabbey@users.noreply.github.com>
2023-02-11 23:35:25 +0100

    navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -16,6 +16,10 @@ const NavBar = () => {
         window.addEventListener("scroll", onScroll)
         return () => window.removeEventListener("scroll", onScroll)
     }, [])
+
+    const handleUpdateActiveLink = (value: string) =>{
+        setActiveLink(value)
+    }
   return (
         <Navbar expand="lg" className={ scroll ? "scrolled" : ""}>
 
@@ -28,9 +32,9 @@ const NavBar = () => {
                 </Navbar.Toggle>
                 <Navbar.Collapse>
                     <Nav className="me-auto">
-                        <Nav.Link href='#home' className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'}>Home</Nav.Link>
-                        <Nav.Link href='#skills' className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'}>Skills </Nav.Link>
-                        <Nav.Link href='#projects' className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'}>Projects </Nav.Link>
+                        <Nav.Link href='#home' className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={()=> handleUpdateActiveLink('home')}>Home</Nav.Link>
+                        <Nav.Link href='#skills' className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={()=> handleUpdateActiveLink('skills')}>Skills </Nav.Link>
+                        <Nav.Link href='#projects' className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={()=> handleUpdateActiveLink('projects')}>Projects </Nav.Link>
                     </Nav>
                     <span className="navbar-text">
                         <div className="social-icons">
