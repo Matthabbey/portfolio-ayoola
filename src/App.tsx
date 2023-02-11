c15bece721b94a0592677ddc0cdf1c57b2750079
Akinpelu Abiodun <59386529+Matthabbey@users.noreply.github.com>
2023-02-11 23:06:45 +0100

    app.

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ function App() {
   return (
     <div className="App">
       <div>
-        <NavBar />
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
