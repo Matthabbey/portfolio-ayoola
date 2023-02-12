bc5cb526c2f281a4fb3578800f1673529b710740
Akinpelu Abiodun <59386529+Matthabbey@users.noreply.github.com>
2023-02-12 22:27:03 +0100

    set-up

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -22,33 +22,9 @@ const NavBar = () => {
         setActiveLink(value)
     }
   return (
-        <Navbar expand="lg" className={ scroll ? "scrolled" : ""}>
-
-            <Container>
-                <Navbar.Brand href='#home'>
-                    <img src={""} alt="" />
-                </Navbar.Brand>
-                <Navbar.Toggle>
-                    <span className="navbar-toggle-icon"></span>
-                </Navbar.Toggle>
-                <Navbar.Collapse>
-                    <Nav className="me-auto">
-                        <Nav.Link href='#home' className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={()=> handleUpdateActiveLink('home')}>Home</Nav.Link>
-                        <Nav.Link href='#skills' className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={()=> handleUpdateActiveLink('skills')}>Skills </Nav.Link>
-                        <Nav.Link href='#projects' className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={()=> handleUpdateActiveLink('projects')}>Projects </Nav.Link>
-                    </Nav>
-                    <span className="navbar-text">
-                        <div className="social-icons">
-                            <a href="#"><BsFacebook/></a>
-                            <a href="#"><BsLinkedin/></a>
-                            <a href="#"></a>
-                        <button onClick={()=> console.log()}><span>Let's Connect</span></button>
-                        </div>
-                    </span>
-                </Navbar.Collapse>
-            </Container>
-
-        </Navbar>
+  <div className=' bg-white '>
+    Hello World
+  </div>
   )
 }
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],  theme: {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
     extend: {},
   },
   plugins: [],
