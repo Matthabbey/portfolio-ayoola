839d93117f3624f5a185a4bf6ddc9a50ff8a6e72
Akinpelu Abiodun <59386529+Matthabbey@users.noreply.github.com>
2023-02-14 15:41:06 +0100

    skill

--- src/Components/Skill.tsx
+++ src/Components/Skill.tsx
@@ -7,8 +7,8 @@ const Skill = () => {
   return (
     <div className=" justify-center">
       <hr className="h-px my-8 border-0 dark:bg-gray-900" />
-      <h1 className="text-3xl py-2 items-center text-center">Skill Set</h1>
-      <p className="text-md py-2 leading-6 text-center">
+      <h1 className="text-3xl py-2 items-center text-center text-bgColor ">Skill Set</h1>
+      <p className="text-sm py-4 leading-7 text-center">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. A ipsum
         obcaecati ducimus, deleniti vel iure impedit ullam laboriosam incidunt
         aut sunt placeat cum, voluptatibus commodi! Accusantium praesentium
@@ -16,13 +16,13 @@ const Skill = () => {
       </p>
 
       <div className="">
-        <div className="">
-          <div className="border border-bgShadow p-8 rounded-md max-auto">
+        <div className=" flex justify-center gap-20 items-center mb-8">
+          <div className="border border-bgShadow p-8 flex flex-col  items-center rounded-md max-auto">
             <img src={CodeLogo} alt="" className="w-12 h-12"/>
             <p className="text-lightOverlay text-sm">Over 2years</p>
             <h3>Programmer</h3>
           </div>
-          <div className="border border-bgShadow p-8 rounded-md items-center text-center">
+          <div className="border border-bgShadow p-8 flex flex-col rounded-md items-center text-center">
             <img src={ProjectLogo} alt="" className="w-12 h-12" />
             <p className="text-lightOverlay text-sm">Over 2years</p>
             <h3>Programmer</h3>
@@ -33,17 +33,17 @@ const Skill = () => {
             <h3>Programmer</h3>
           </div>
         </div>
-        <div className=" justify-center border border-bgShadow gap-16 p-1 flex w-[80%] mx-[auto]">
+        <div className=" justify-center border border-bgShadow gap-16 p-0 flex w-[50%] mx-[auto] rounded-md">
           <div>
-            <p className="text-center text-sm">Developer</p>
+            <p className="text-center text-lightOverlay">Developer</p>
             <h3 className="text-bgColor text-center">Full-Stack Developer</h3>
           </div>
           <div>
-            <p className="text-center">Developer</p>
+            <p className="text-center text-lightOverlay">Developer</p>
             <h3 className="text-bgColor text-center">Full-Stack Developer</h3>
           </div>
           <div>
-            <p className="text-center">Developer</p>
+            <p className="text-center text-lightOverlay">Developer</p>
             <h3 className="text-bgColor text-center">Full-Stack Developer</h3>
           </div>
         </div>
