ebb9488d260221d2a003e510c8443ab4293b0f44
Akinpelu Abiodun <59386529+Matthabbey@users.noreply.github.com>
2023-02-14 16:55:14 +0100

    card

--- src/Components/Cards.tsx
+++ src/Components/Cards.tsx
@@ -1,19 +1,17 @@
 import React from 'react'
+import ProfileLogo from '../public/favicon_io/ProfilePic.png'
 
 const Cards = () => {
   return (
-    <div>
-      <hr className="h-px my-8 border-0 dark:bg-gray-900" />
+    <div className='flex gap-20'>
       <div>
-        <img src="" alt="" />
+        <img src={ProfileLogo} alt="" />
       </div>
       <div>
         <h4>About me!</h4>
         <h1>Akinpelu Abiodun MAtthew</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum ad obcaecati inventore natus sed repudiandae temporibus, dignissimos maxime delectus suscipit consequatur sequi magni laboriosam, deserunt dolores debitis itaque numquam illum.</p>     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum ad obcaecati inventore natus sed repudiandae temporibus, dignissimos maxime delectus suscipit consequatur sequi magni laboriosam, deserunt dolores debitis itaque numquam illum.</p>     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum ad obcaecati inventore natus sed repudiandae temporibus, dignissimos maxime delectus suscipit consequatur sequi magni laboriosam, deserunt dolores debitis itaque numquam illum.</p>     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum ad obcaecati inventore natus sed repudiandae temporibus, dignissimos maxime delectus suscipit consequatur sequi magni laboriosam, deserunt dolores debitis itaque numquam illum.</p>
+        <p className="[content:'\1F44B']">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum ad obcaecati inventore natus sed repudiandae temporibus, dignissimos maxime delectus suscipit consequatur sequi magni laboriosam, deserunt dolores debitis itaque numquam illum.</p>     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum ad obcaecati inventore natus sed repudiandae temporibus, dignissimos maxime delectus suscipit consequatur sequi magni laboriosam, deserunt dolores debitis itaque numquam illum.</p>     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum ad obcaecati inventore natus sed repudiandae temporibus, dignissimos maxime delectus suscipit consequatur sequi magni laboriosam, deserunt dolores debitis itaque numquam illum.</p>     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum ad obcaecati inventore natus sed repudiandae temporibus, dignissimos maxime delectus suscipit consequatur sequi magni laboriosam, deserunt dolores debitis itaque numquam illum.</p>
       </div>
-
-        Cards
     </div>
   )
 }
