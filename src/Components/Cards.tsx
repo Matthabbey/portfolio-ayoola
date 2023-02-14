0292246d2f1c08e2373893ec8b69307634f94b06
Akinpelu Abiodun <59386529+Matthabbey@users.noreply.github.com>
2023-02-14 16:20:31 +0100

    cards

--- src/Components/Cards.tsx
+++ src/Components/Cards.tsx
@@ -7,6 +7,11 @@ const Cards = () => {
       <div>
         <img src="" alt="" />
       </div>
+      <div>
+        <h4>About me!</h4>
+        <h1>Akinpelu Abiodun MAtthew</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum ad obcaecati inventore natus sed repudiandae temporibus, dignissimos maxime delectus suscipit consequatur sequi magni laboriosam, deserunt dolores debitis itaque numquam illum.</p>     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum ad obcaecati inventore natus sed repudiandae temporibus, dignissimos maxime delectus suscipit consequatur sequi magni laboriosam, deserunt dolores debitis itaque numquam illum.</p>     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum ad obcaecati inventore natus sed repudiandae temporibus, dignissimos maxime delectus suscipit consequatur sequi magni laboriosam, deserunt dolores debitis itaque numquam illum.</p>     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum ad obcaecati inventore natus sed repudiandae temporibus, dignissimos maxime delectus suscipit consequatur sequi magni laboriosam, deserunt dolores debitis itaque numquam illum.</p>
+      </div>
 
         Cards
     </div>
