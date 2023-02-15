a392247f7b6df7623635112ed7c55a22fd0ce85f
Akinpelu Abiodun <59386529+Matthabbey@users.noreply.github.com>
2023-02-16 00:42:32 +0100

    project

--- src/Components/Cards.tsx
+++ src/Components/Cards.tsx
@@ -10,7 +10,7 @@ const Cards = () => {
         <img src={ProfileLogo} alt="" width={200} height={200} className="ml-[7%] py-20" />
 
         <div className="space-y-2 py-[10%] px-[10%] mr-[5%]">
-          <h4 className="border border-bgColor py-1 px-2 rounded-md text-cardOverlay">About me!</h4>
+          <h4 className="border border-bgColor w-[25%] text-center py-1 px-2 rounded-md text-cardOverlay">About me!</h4>
           <h1 className="font-extrabold ">Akinpelu Abiodun Matthew</h1>
           <p className="[content:'\1F44B'] text-sm font-thin text-cardOverlay">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum ad
--- src/Components/Project.tsx
+++ src/Components/Project.tsx
@@ -1,39 +1,88 @@
-import React from 'react' 
-import CodeLogo from '../public/CodeIcon.svg'
-import ProjectLogo from '../public/ProjectsIcon.svg'
-import DesignLogo from '../public/DesignIcon.svg'
-
+import React from "react";
+import CodeLogo from "../public/CodeIcon.svg";
+import ProjectLogo from "../public/ProjectsIcon.svg";
+import DesignLogo from "../public/DesignIcon.svg";
+import ProjectImg from "../public/image.png"
 
 const Project = () => {
   return (
     <div>
-            <h1>Portfolio</h1>
+      <h1>Portfolio</h1>
+      <div>
+        <p>List of Projects Completed</p>
         <div>
-            <p>List of Projects Completed</p>
-            <div>
-                <div>
-                    <img src={DesignLogo} alt="" />
-                    <span>UI Design</span>
-                </div>
-                <div>
-                    <img src={ProjectLogo} alt="" />
-                    <span>UI Design</span>
-                </div>
-                <div>
-                    <img src={CodeLogo} alt="" />
-                    <span>UI Design</span>
-                </div>
-            </div>
+          <div>
+            <img src={DesignLogo} alt="" />
+            <span>UI Design</span>
+          </div>
+          <div>
+            <img src={ProjectLogo} alt="" />
+            <span>UI Design</span>
+          </div>
+          <div>
+            <img src={CodeLogo} alt="" />
+            <span>UI Design</span>
+          </div>
         </div>
+      </div>
 
+      <div className="flex gap-5 justify-center ">
+        <div className="border border-bgShadow w-[30%] px-8 rounded-md py-5">
+          <p className="font-extrabold pb-2">Move.it</p>
+          <p className="text-cardOverlay pb-8">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus,
+            nemo. Odit in, voluptatem praesentium sint mollitia
+          </p>
+          <div className="flex py-5 gap-2 mb-0">
+            <button className="border border-bgColor w-[30%] text-center py-0 px-0 rounded-md text-cardOverlay">
+              Online
+            </button>
+            <button className="border border-bgColor w-[40%] text-center py- px- rounded-md text-cardOverlay">
+              TypeScript
+            </button>
+          </div>
+            <img src={ProjectImg} alt="" className="" />
+        </div>
         <div>
-            <div>
-                <p>Move.it</p>
-            </div>
+        <div className="border border-bgShadow w-[30%] px-8 rounded-md py-5">
+          <p className="font-extrabold pb-2">Move.it</p>
+          <p className="text-cardOverlay pb-8">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus,
+            nemo. Odit in, voluptatem praesentium sint mollitia
+          </p>
+          <div className="flex py-5 gap-2 mb-0">
+            <button className="border border-bgColor w-[30%] text-center py-0 px-0 rounded-md text-cardOverlay">
+              Online
+            </button>
+            <button className="border border-bgColor w-[40%] text-center py- px- rounded-md text-cardOverlay">
+              TypeScript
+            </button>
+          </div>
+            <img src={ProjectImg} alt="" className="" />
         </div>
-
+      </div>
+      <div>
+        <div className="border border-bgShadow w-[30%] px-8 rounded-md py-5">
+          <p className="font-extrabold pb-2">Move.it</p>
+          <p className="text-cardOverlay pb-8">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus,
+            nemo. Odit in, voluptatem praesentium sint mollitia
+          </p>
+          <div className="flex py-5 gap-2 mb-0">
+            <button className="border border-bgColor w-[30%] text-center py-0 px-0 rounded-md text-cardOverlay">
+              Online
+            </button>
+            <button className="border border-bgColor w-[40%] text-center py- px- rounded-md text-cardOverlay">
+              TypeScript
+            </button>
+          </div>
+            <img src={ProjectImg} alt="" className="" />
+        </div>
+      </div>
+      </div>
+      
     </div>
-  )
-}
+  );
+};
 
-export default Project
+export default Project;
