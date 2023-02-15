22afcc54394722d252d8efa409dd254eff7d4bee
Akinpelu Abiodun <59386529+Matthabbey@users.noreply.github.com>
2023-02-15 09:55:44 +0100

    raleway

--- src/Components/Cards.tsx
+++ src/Components/Cards.tsx
@@ -6,22 +6,22 @@ const Cards = () => {
     <div>
       <hr className="h-px my-8 border-0 dark:bg-gray-900 mt-10" />
 
-      <div className="gap-[20px] flex mt-[5%] ml-[5%]">
-        <img src={ProfileLogo} alt="" width={400} height={350} />
+      <div className="gap-[90px] flex py-[2%] ml-[5%]">
+        <img src={ProfileLogo} alt="" width={200} height={100} className="ml-[7%] py-10" />
 
-        <div className="space-y-4 mt-[5%] px-[10%] mr-[5%]">
+        <div className="space-y-2 py-[3%] px-[10%] mr-[5%]">
           <h4 className="border border-bgColor py-1 px-2 rounded-md text-cardOverlay">About me!</h4>
-          <h1>Akinpelu Abiodun MAtthew</h1>
-          <p className="[content:'\1F44B'] text-sm font-thin">
+          <h1 className="font-extrabold ">Akinpelu Abiodun Matthew</h1>
+          <p className="[content:'\1F44B'] text-sm font-thin text-cardOverlay">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum ad
           </p>
-          <p className="text-sm font-thin">
+          <p className="text-sm font-thin text-cardOverlay">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum ad
           </p>{" "}
-          <p className="text-sm font-thin">
+          <p className="text-sm font-thin text-cardOverlay">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum ad
           </p>{" "}
-          <p className="text-sm font-thin">
+          <p className="text-sm font-thin text-cardOverlay">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum ad
           </p>
         </div>
