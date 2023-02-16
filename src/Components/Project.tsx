52a7656bb251b4c654365c9fc3ad39dac3bf5e02
Akinpelu Abiodun <59386529+Matthabbey@users.noreply.github.com>
2023-02-16 01:23:47 +0100

    grid

--- src/Components/Project.tsx
+++ src/Components/Project.tsx
@@ -26,35 +26,35 @@ const Project = () => {
         </div>
       </div>
 
-      <div className="flex gap-5 justify-center ">
-        <div className="border border-bgShadow w-[30%] px-8 rounded-md py-5">
+      <div className="px-[5%] gap-5 grid grid-cols-4">
+        <div className="border border-bgShadow w-[100%] px-8 rounded-md py-5">
           <p className="font-extrabold pb-2">Move.it</p>
           <p className="text-cardOverlay pb-8">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus,
-            nemo. Odit in, voluptatem praesentium sint mollitia
+            nemo. Odit in
           </p>
           <div className="flex py-5 gap-2 mb-0">
-            <button className="border border-bgColor w-[30%] text-center py-0 px-0 rounded-md text-cardOverlay">
+            <button className=" bg-green-800 w-[30%] text-center font-sans py-0 px-0 rounded-md text-white">
               Online
             </button>
-            <button className="border border-bgColor w-[40%] text-center py- px- rounded-md text-cardOverlay">
-              TypeScript
+            <button className="border border-bgColor w-[45%] text-center py- px- rounded-md text-cardOverlay">
+              TypeScript 
             </button>
           </div>
             <img src={ProjectImg} alt="" className="" />
         </div>
         <div>
-        <div className="border border-bgShadow w-[30%] px-8 rounded-md py-5">
+        <div className="border border-bgShadow w-[100%] px-8 rounded-md py-5">
           <p className="font-extrabold pb-2">Move.it</p>
           <p className="text-cardOverlay pb-8">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus,
-            nemo. Odit in, voluptatem praesentium sint mollitia
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus,
+            nemo. Odit in
           </p>
           <div className="flex py-5 gap-2 mb-0">
             <button className="border border-bgColor w-[30%] text-center py-0 px-0 rounded-md text-cardOverlay">
               Online
             </button>
-            <button className="border border-bgColor w-[40%] text-center py- px- rounded-md text-cardOverlay">
+            <button className="border border-bgColor w-[45%] text-center py- px- rounded-md text-cardOverlay">
               TypeScript
             </button>
           </div>
@@ -62,17 +62,105 @@ const Project = () => {
         </div>
       </div>
       <div>
-        <div className="border border-bgShadow w-[30%] px-8 rounded-md py-5">
+        <div className="border border-bgShadow w-[100%] px-8 rounded-md py-5">
           <p className="font-extrabold pb-2">Move.it</p>
           <p className="text-cardOverlay pb-8">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus,
-            nemo. Odit in, voluptatem praesentium sint mollitia
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus,
+            nemo. Odit in
           </p>
           <div className="flex py-5 gap-2 mb-0">
             <button className="border border-bgColor w-[30%] text-center py-0 px-0 rounded-md text-cardOverlay">
               Online
             </button>
-            <button className="border border-bgColor w-[40%] text-center py- px- rounded-md text-cardOverlay">
+            <button className="border border-bgColor w-[45%] text-center py- px- rounded-md text-cardOverlay">
+              TypeScript
+            </button>
+          </div>
+            <img src={ProjectImg} alt="" className="" />
+        </div>
+      </div>
+      <div>
+        <div className="border border-bgShadow w-[100%] px-8 rounded-md py-5">
+          <p className="font-extrabold pb-2">Move.it</p>
+          <p className="text-cardOverlay pb-8">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus,
+            nemo. Odit in
+          </p>
+          <div className="flex py-5 gap-2 mb-0">
+            <button className="border border-bgColor w-[30%] text-center py-0 px-2 rounded-md text-cardOverlay">
+              Online
+            </button>
+            <button className="border border-bgColor w-[45%] text-center py- px- rounded-md text-cardOverlay">
+              TypeScript
+            </button>
+          </div>
+            <img src={ProjectImg} alt="" className="" />
+        </div>
+      </div>
+      <div className="border border-bgShadow w-[100%] px-8 rounded-md py-5">
+          <p className="font-extrabold pb-2">Move.it</p>
+          <p className="text-cardOverlay pb-8">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus,
+            nemo. Odit in
+          </p>
+          <div className="flex py-5 gap-2 mb-0">
+            <button className="border border-bgColor w-[30%] text-center py-0 px-0 rounded-md text-cardOverlay">
+              Online
+            </button>
+            <button className="border border-bgColor w-[45%] text-center py- px- rounded-md text-cardOverlay">
+              TypeScript
+            </button>
+          </div>
+            <img src={ProjectImg} alt="" className="" />
+        </div>
+        <div>
+        <div className="border border-bgShadow w-[100%] px-8 rounded-md py-5">
+          <p className="font-extrabold pb-2">Move.it</p>
+          <p className="text-cardOverlay pb-8">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus,
+            nemo. Odit in
+          </p>
+          <div className="flex py-5 gap-2 mb-0">
+            <button className="border border-bgColor w-[30%] text-center py-0 px-0 rounded-md text-cardOverlay">
+              Online
+            </button>
+            <button className="border border-bgColor w-[45%] text-center py- px- rounded-md text-cardOverlay">
+              TypeScript
+            </button>
+          </div>
+            <img src={ProjectImg} alt="" className="" />
+        </div>
+      </div>
+      <div>
+        <div className="border border-bgShadow w-[100%] px-8 rounded-md py-5">
+          <p className="font-extrabold pb-2">Move.it</p>
+          <p className="text-cardOverlay pb-8">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus,
+            nemo. Odit in
+          </p>
+          <div className="flex py-5 gap-2 mb-0">
+            <button className="border border-bgColor w-[30%] text-center py-0 px-0 rounded-md text-cardOverlay">
+              Online
+            </button>
+            <button className="border border-bgColor w-[45%] text-center py- px- rounded-md text-cardOverlay">
+              TypeScript
+            </button>
+          </div>
+            <img src={ProjectImg} alt="" className="" />
+        </div>
+      </div>
+      <div>
+        <div className="border border-bgShadow w-[100%] px-8 rounded-md py-5">
+          <p className="font-extrabold pb-2">Move.it</p>
+          <p className="text-cardOverlay pb-8">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus,
+            nemo. Odit in
+          </p>
+          <div className="flex py-5 gap-2 mb-0">
+            <button className="border border-bgColor w-[30%] text-center py-0 px-2 rounded-md text-cardOverlay">
+              Online
+            </button>
+            <button className="border border-bgColor w-[45%] text-center py- px- rounded-md text-cardOverlay">
               TypeScript
             </button>
           </div>
@@ -81,6 +169,8 @@ const Project = () => {
       </div>
       </div>
       
+      <hr className="h-px my-8 border-0 dark:bg-gray-900 mt-10" />
+
     </div>
   );
 };
