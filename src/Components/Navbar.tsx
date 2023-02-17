9af92a3340ca7bcc1f2489992073ee6d0e956cc9
Akinpelu Abiodun <59386529+Matthabbey@users.noreply.github.com>
2023-02-18 00:58:09 +0100

    updated

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import Logo from "../public/Logo.svg";
 const NavBar = () => {
   const [activeLink, setActiveLink] = useState("home");
