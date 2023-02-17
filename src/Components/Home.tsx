9304a689c0f1cf6132c38122d58bf39473248132
Akinpelu Abiodun <59386529+Matthabbey@users.noreply.github.com>
2023-02-17 09:42:38 +0100

    update

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -11,14 +11,17 @@ const Home = () => {
         </h1>
         <p className="py-0">Full-Stack Developer </p>
         <div className="text-2xl flex gap-5 py-2 rounded w-50 h-50 cursor-pointer">
-          <BsLinkedin />
-          <BsGithub />
+          <BsLinkedin onClick={()=>window.open("https://www.linkedin.com/in/abiodun-akinpelu-20147a176/")}
+ />
+          <BsGithub  onClick={() => window.open("https://github.com/matthabbey")}/>
           <BsInstagram />
         </div>
       </div>
       <img src={HeadIcon} alt="" width={600} height={500} />
 
-      <div className="text-xl flex gap-2 items-center text-cardOverlay cursor-pointer rounded-lg border border-bgShadow p-2">
+      <div className="text-xl flex gap-2 items-center text-cardOverlay cursor-pointer rounded-lg border border-bgShadow p-2"    onClick={() =>
+        window.open("https://api.whatsapp.com/send/?phone=08036202050")
+      }>
         <BsWhatsapp className="text-green-800"/>
         <h3>Whatsapp!</h3>
       </div>
